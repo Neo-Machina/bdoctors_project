f2542687120fa28958665fd6d7149cc3d44defcd
cibs84 <d.tilesi@gmail.com>
2022-09-27 18:00:51 +0200

    aggiunta graffa in user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,25 +20,37 @@ class UsersTableSeeder extends Seeder
         for($i = 0; $i < 150; $i++) {
             $new_user = new User();
 
-            $new_user->name = $faker->name($gender = 'male'|'female'); 
+            $new_user->name = $faker->firstNameMale(); 
             $new_user->address = $faker->streetAddress(); 
             $new_user->phone_number = $faker->e164PhoneNumber();
             $new_user->email = $faker->email();
-            $new_user->curriculum = $faker->text(3000);
+            $new_user->curriculum = $faker->paragraphs(4, true);
             $new_user->photo = 'https://s3-eu-west-1.amazonaws.com/miodottore.it/doctor/b26aee/b26aee7167aa5d475a7761d55f2e6bbd_large.jpg';
             $new_user->slug = $this->getFreeSlugFromTitle($new_user->name);
             $new_user->password = 'password123';
-            $new_user->service = implode(', ', $faker->words(rand(5,15)));
+            $new_user->service = $faker->words(rand(5, 15), true);
             $new_user->save();
+
+            // $specialization_ids = []; 
+            // $specializations_array = Specialization::all();
+
+            // for($i=1; $i < rand(1, 3); $i++) { 
+            //     $id_random = rand(1, count($specializations_array));
+
+            //     if(!in_array( $id_random, $specialization_ids)) {
+            //         $specialization_ids[] = $id_random;
+            //     } 
+            // }
 
             $specialization_ids = []; 
 
-            for($j = 1; $j <= rand(1, 3); $j++) { 
+            for($i=1; $i <= rand(1, 3); $i++) { 
                 $id_random = rand(1, 8);
 
                 if(!in_array( $id_random, $specialization_ids)) {
                     $specialization_ids[] = $id_random;
                 } 
+
             }
 
             $new_user->specializations()->sync($specialization_ids);
@@ -67,4 +79,3 @@ class UsersTableSeeder extends Seeder
         return $slug_to_save;
     }
 }
-
