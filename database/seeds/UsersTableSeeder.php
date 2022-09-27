164220cc6cb90e5c49fae90646ddc523862b24a8
Neo-Machina <andreistatache1997@gmail.com>
2022-09-27 17:32:41 +0200

    user seeder fixed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,63 +16,50 @@ class UsersTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        // DB::table('users')->truncate();
 
-        // for($i = 0; $i < 150; $i++) {
-        //     $new_user = new User();
+        for($i = 0; $i < 150; $i++) {
+            $new_user = new User();
 
-        //     $new_user->name = $faker->firstNameMale(); 
-        //     $new_user->address = $faker->streetAddress(); 
-        //     $new_user->phone_number = $faker->e164PhoneNumber();
-        //     $new_user->email = $faker->email();
-        //     $new_user->curriculum = $faker->paragraphs(4, true);
-        //     $new_user->photo = 'https://s3-eu-west-1.amazonaws.com/miodottore.it/doctor/b26aee/b26aee7167aa5d475a7761d55f2e6bbd_large.jpg';
-        //     $new_user->slug = $this->getFreeSlugFromTitle($new_user->name);
-        //     $new_user->password = 'password123';
-        //     $new_user->service = $faker->words(rand(5, 15), true);
-        //     $new_user->save();
-
-<<<<<<< HEAD
-
-        //     $specialization_ids = []; 
-        //     $specializations_array = Specialization::all();
-
-        //     for($i=1; $i < rand(1, 3); $i++) { 
-        //         $id_random = rand(1, count($specializations_array));
-
-        //         if(!in_array( $id_random, $specialization_ids)) {
-        //             $specialization_ids[] = $id_random;
-        //         } 
-        //     }
-
-        //     $new_user->specializations()->sync([$specialization_ids]);
-        // }
-=======
-            // $specialization_ids = []; 
-            // $specializations_array = Specialization::all();
-
-            // for($i=1; $i < rand(1, 3); $i++) { 
-            //     $id_random = rand(1, count($specializations_array));
-
-            //     if(!in_array( $id_random, $specialization_ids)) {
-            //         $specialization_ids[] = $id_random;
-            //     } 
-            // }
+            $new_user->name = $faker->name($gender = 'male'|'female'); 
+            $new_user->address = $faker->streetAddress(); 
+            $new_user->phone_number = $faker->e164PhoneNumber();
+            $new_user->email = $faker->email();
+            $new_user->curriculum = $faker->text(3000);
+            $new_user->photo = 'https://s3-eu-west-1.amazonaws.com/miodottore.it/doctor/b26aee/b26aee7167aa5d475a7761d55f2e6bbd_large.jpg';
+            $new_user->slug = $this->getFreeSlugFromTitle($new_user->name);
+            $new_user->password = 'password123';
+            $new_user->service = implode(', ', $faker->words(rand(5,15)));
+            $new_user->save();
 
             $specialization_ids = []; 
 
-            for($i=1; $i <= rand(1, 3); $i++) { 
+            for($j = 1; $j <= rand(1, 3); $j++) { 
                 $id_random = rand(1, 8);
 
                 if(!in_array( $id_random, $specialization_ids)) {
                     $specialization_ids[] = $id_random;
                 } 
-
             }
 
             $new_user->specializations()->sync($specialization_ids);
         }
->>>>>>> 7cd6578cae0878bfceb85e95e52eee158ac0ba5a
+
+
+
+
+        // factory(App\User::class, 150)->create([
+        //     'name' => $faker->name(),
+        //     'address' => $faker->address(),
+        //     'phone_number' => $faker->e164PhoneNumber(),
+        //     'email' => $faker->email(),
+        //     'curriculum' => $faker->paragraphs(4, true),
+        //     'photo' => 'https://s3-eu-west-1.amazonaws.com/miodottore.it/doctor/b26aee/b26aee7167aa5d475a7761d55f2e6bbd_large.jpg',
+        //     'slug' => $this->getFreeSlugFromTitle($user->name),
+        //     'password' => 'password123',
+        //     'service' => $faker->words(rand(5, 15), true)
+        // ])->each(function($user) {
+        //     $user->specializations()->saveMany(factory(App\Specialization::class, rand(1,3))->create());
+        // });
     }
 
     protected function getFreeSlugFromTitle($name) {
