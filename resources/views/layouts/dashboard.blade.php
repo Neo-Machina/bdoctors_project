9c30805c2a77c2ec54f0bf603567a26b797f7f26
azzurragraziano <azzurragraziano02@email.com>
2022-09-28 16:20:49 +0200

    home controller

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -12,6 +12,60 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark flex-md-nowrap p-0">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BDoctors</a>
+        <ul class="navbar-nav px-3 ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar py-4">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+
+                        {{-- dashboard link --}}
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ route('admin.home') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Dashboard
+                            </a>
+                        </li>
+
+                        {{-- user profile link
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ route('admin.posts.index') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>     
+                                Posts
+                            </a>
+                        </li>
+
+                        {{-- create a new post link 
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ route('admin.posts.create') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Create a new post
+                            </a>
+                        </li> --}}
+                    </ul>
+
+                </div>
+            </nav>
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 py-4">
+                @yield('content')
+            </main>
+        </div>
+    </div>
 </body>
 </html>
