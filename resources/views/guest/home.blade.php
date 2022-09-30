5a8b96a71dfb17a41b82bb1c4131b4231a630bdb
azzurragraziano <azzurragraziano02@email.com>
2022-09-30 15:46:47 +0200

    home.blade

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,13 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     {{-- Scripts --}}
     <script src="{{ asset('js/popper.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
-=======
->>>>>>> init-vue
     <title>Document</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
