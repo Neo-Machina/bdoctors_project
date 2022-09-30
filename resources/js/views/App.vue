887b652a02dcbd8fb30269d557e9d2ce8026a747
azzurragraziano <azzurragraziano02@email.com>
2022-09-30 19:56:04 +0200

    homepage, init card-profile

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -2,20 +2,24 @@
     <div>
         <Header/>
 
-   
         <main>
             <router-view></router-view>
         </main>
+
+        <!-- <Footer/> -->
+
     </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
+// import Footer from '../components/Footer.vue';
 
 export default {
     name: 'App',
     components: {
-        Header
+        Header,
+        // Footer
     }
 }
 </script>
