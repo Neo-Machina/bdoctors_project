862ecf56bb89b3c8593cbf313c962398fc95030e
Neo-Machina <andreistatache1997@gmail.com>
2022-09-30 11:17:38 +0200

    popper.js file added in resource folder

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- Scripts --}}
+    <script src="{{ asset('js/popper.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     <title>Document</title>
 </head>
 <body>
