7de97a5dc7308b203f98dc3ab251934bd99a0989
cibs84 <d.tilesi@gmail.com>
2022-10-05 14:05:58 +0200

    getSpecializationUser partial refactoring

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,6 +8,8 @@ use App\User;
 use Illuminate\Support\Facades\DB;
 use App\Specialization;
 use Illuminate\Support\Carbon;
+use App\Review;
+use App\UserBundle;
 
 class UserController extends Controller
 {   
@@ -52,20 +54,25 @@ class UserController extends Controller
     // mantenendo i record con la scadenza bundle più recente (orderBy('user_bundle.expired_date', 'desc')) 
     // e li ordina in modo che chi è sponsorizzato sia mostrato per primo (orderBy('user_bundle.expired_date', 'desc'))
     public function getUsersBySpecialization($specialization_slug) {
-        $users = User::select('users.id', 'users.name as user_name', 'users.email as user_email', 'users.address as user_address', 'users.photo as user_photo', 'users.slug as user_slug', 'users.phone_number as user_phone_number', 'users.service as user_service', 'users.curriculum as user_curriculum', 
-                             'specializations.id as specialization_id', 'specializations.name as specialization_name', 'specializations.slug as specialization_slug',
-                             'user_bundle.expired_date', 'bundles.id as bundle_id', 'bundles.name as bundle_name', 'bundles.duration as bundle_duration', 'bundles.price as bundle_price', 'bundles.code as bundle_code',
-                             'reviews.id as review_id', 'reviews.author as review_author', 'reviews.content as review_content', 'reviews.vote as review_vote', 'reviews.created_at as review_created_at', 
-                            //  'messages.id as message_id', 'messages.author as message_author', 'messages.email as message_email', 'messages.content as message_content', 'messages.created_at as message_created_at',
-                             DB::raw('count(reviews.user_id) as reviews_count'),
-                             DB::table('users')->raw('avg(reviews.vote) as reviews_avg'))
-                       ->join('reviews', 'users.id', '=', 'reviews.user_id')->groupBy('reviews.user_id')
-                    //    ->join('messages', 'messages.id', '=', 'messages.user_id')
-                       ->join('specialization_user', 'users.id', '=', 'specialization_user.user_id')
-                       ->join('specializations', 'specializations.id', '=', 'specialization_id')->having('specializations.slug', '=', $specialization_slug)
-                       ->leftJoin('user_bundle', 'users.id', '=', 'user_bundle.user_id')
-                       ->leftJoin('bundles', 'bundles.id', '=', 'bundle_id')->groupBy('users.id')->orderBy('user_bundle.expired_date', 'desc') 
-                       ->paginate(3000);
+        // $users = User::select('users.id', 'users.name as user_name', 'users.email as user_email', 'users.address as user_address', 'users.photo as user_photo', 'users.slug as user_slug', 'users.phone_number as user_phone_number', 'users.service as user_service', 'users.curriculum as user_curriculum', 
+        //                      'reviews.id as review_id', 'reviews.author as review_author', 'reviews.content as review_content', 'reviews.vote as review_vote', 'reviews.created_at as review_created_at', 
+        //                      DB::table('users')->raw('avg(reviews.vote) as reviews_avg'),
+        //                      DB::raw('count(reviews.user_id) as reviews_count'),)
+
+        //                     //  'specializations.id as specialization_id', 'specializations.name as specialization_name', 'specializations.slug as specialization_slug',
+        //                     //  'user_bundle.expired_date', 'bundles.id as bundle_id', 'bundles.name as bundle_name', 'bundles.duration as bundle_duration', 'bundles.price as bundle_price', 'bundles.code as bundle_code',
+        //                     //  'messages.id as message_id', 'messages.author as message_author', 'messages.email as message_email', 'messages.content as message_content', 'messages.created_at as message_created_at',
+                       
+        //                     ->join('reviews', 'users.id', '=', 'reviews.user_id')->groupBy('reviews.user_id')
+
+        //             //    ->join('messages', 'messages.id', '=', 'messages.user_id')
+        //             //    ->join('specialization_user', 'users.id', '=', 'specialization_user.user_id')
+        //             //    ->join('specializations', 'specializations.id', '=', 'specialization_id')->having('specializations.slug', '=', $specialization_slug)
+        //             //    ->leftJoin('user_bundle', 'users.id', '=', 'user_bundle.user_id')
+        //             //    ->leftJoin('bundles', 'bundles.id', '=', 'user_bundle.bundle_id')->groupBy('users.id')->orderBy('user_bundle.expired_date', 'desc') 
+                       
+        //                     ->paginate(3000);
+
 
         // $users = DB::table('users')
         //             ->rightJoin('reviews', 'users.id', '=', 'reviews.user_id')->groupBy('reviews.user_id')
@@ -87,14 +94,29 @@ class UserController extends Controller
         // dd($users_ordered_by_expired_date[0]->pivot->expired_date);
         
         // METODO CORRETTO
-        // $users = User::table('users')->select('users.*', 'user_bundle.expired_date')->join('bundles', 'bundles.id', '=', 'bundle_id')->orderBy('user_bundle.expired_date', 'desc')->join('specializations', 'specializations.id', '=', 'specialization_id')->having('specializations.slug', '=', $specialization_slug)->get();
+        $users = User::select('users.*', 'user_bundle.expired_date', 'specializations.slug as specialization_slug')
+                    ->join('user_bundle', 'users.id', '=', 'user_bundle.user_id')
+                    ->join('bundles', 'bundles.id', '=', 'user_bundle.bundle_id')//->orderBy('user_bundle.expired_date', 'desc')->groupBy('users.id')
+                    ->join('specialization_user', 'users.id', '=', 'specialization_user.user_id')
+                    ->join('specializations', 'specializations.id', '=', 'specialization_user.specialization_id')->having('specializations.slug', '=', $specialization_slug)
+                    ->get();
 
-        //     foreach($users as $user) {
-        //         $reviews = Reviews::select(reviews.*, count(reviews.user_id) as review_number)->where('reviews.user_id', '=', $user.id)->get();
-        //         $bundles = UserBundles::select(user_bundles.*)->where('user_bundles.user_id', '=', $user.id)->get();
-        //         $user['reviews'] = $reviews;
-        //         $user['bundles'] = $bundles;
-        //     }
+            foreach($users as $user) {
+                // $reviews = Review::select('reviews.*', 'count(reviews.user_id) as review_number')->where('reviews.user_id', '=', $user->id)->get();
+                $reviews = DB::table('reviews')
+                            ->select('reviews.*', DB::raw('count(reviews.user_id) as reviews_count'), DB::raw('avg(reviews.vote) as reviews_avg_vote'))->where('reviews.user_id', '=', $user->id)->get();
+                // ->select(array('issues.*', DB::raw('COUNT(issue_subscriptions.issue_id) as followers')))
+                $bundles = UserBundle::select('user_bundle.*')->where('user_bundle.user_id', '=', $user->id)->orderBy('user_bundle.expired_date', 'desc')->get();
+                // $user['reviews_count'] = $reviews['reviews_count'];
+                $user['reviews_count'] = $reviews[0]->reviews_count;
+                $user['reviews_avg_vote'] = $reviews[0]->reviews_avg_vote;
+                $user['expired_date_bundle'] = $bundles[0]->expired_date;
+                // dump($reviews[0]->reviews_count);
+                // $user['reviews'] = $reviews;
+                // $user['bundles'] = $bundles;
+            }
+            dd($users[0][0]);
+            $users = $users->groupBy($this);
 
         if (isset($users)) {
             $data = [
